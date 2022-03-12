057141bb4a689fa9d647852d6d4490527ca49285
Ben Green <ben@bcgdesign.com>
2022-03-12 16:41:23 +0000

    Adding tests

---
+++ tests/Tests.Rnd/Generators/Rnd/GuidF/Get_Tests.cs
@@ -0,0 +1,26 @@
+﻿// Rnd Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2021
+
+namespace Rnd.Generators.Rnd_Tests.GuidF_Tests;
+
+public class Get_Tests
+{
+	[Fact]
+	public void Returns_Different_Bytes_Each_Time()
+	{
+		// Arrange
+		const int iterations = 10000;
+		var numbers = new List<Guid>();
+
+		// Act
+		for (int i = 0; i < iterations; i++)
+		{
+			numbers.Add(Rnd.GuidF.Get());
+		}
+
+		var unique = numbers.Distinct();
+
+		// Assert
+		Assert.Equal(unique.Count(), numbers.Count);
+	}
+}
