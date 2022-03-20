97cfeb918d2ff062fe65623a4404bb2b8ae84980
Ben Green <ben@bcgdesign.com>
2022-03-20 15:05:48 +0000

    Changing test for word lists to strings - see #14

--- tests/Tests.RndF/Properties/Resources/LongWordList_Tests.cs
+++ tests/Tests.RndF/Properties/Resources/LongWordList_Tests.cs
@@ -1,4 +1,4 @@
-﻿// Rnd: Unit Tests
+// Rnd: Unit Tests
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2021
 
 namespace RndF.Properties.Resources_Tests;
@@ -6,7 +6,7 @@ namespace RndF.Properties.Resources_Tests;
 public class LongWordList_Tests
 {
 	[Fact]
-	public void Returns_Byte_Array()
+	public void Returns_String()
 	{
 		// Arrange
 
@@ -14,6 +14,6 @@ public class LongWordList_Tests
 		var result = Resources.eff_long_word_list;
 
 		// Assert
-		Assert.IsType<byte[]>(result);
+		Assert.IsType<string>(result);
 	}
 }
--- tests/Tests.RndF/Properties/Resources/ShortWordList_Tests.cs
+++ tests/Tests.RndF/Properties/Resources/ShortWordList_Tests.cs
@@ -1,4 +1,4 @@
-﻿// Rnd: Unit Tests
+// Rnd: Unit Tests
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2021
 
 namespace RndF.Properties.Resources_Tests;
@@ -6,7 +6,7 @@ namespace RndF.Properties.Resources_Tests;
 public class ShortWordList_Tests
 {
 	[Fact]
-	public void Returns_Byte_Array()
+	public void Returns_String()
 	{
 		// Arrange
 
@@ -14,6 +14,6 @@ public class ShortWordList_Tests
 		var result = Resources.eff_short_word_list;
 
 		// Assert
-		Assert.IsType<byte[]>(result);
+		Assert.IsType<string>(result);
 	}
 }
