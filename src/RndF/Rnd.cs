8e2704432a075ff913dafedcfbbe4967b677c94e
Ben Green <ben@bcgdesign.com>
2022-03-27 08:41:43 +0100

    Adding Rnd.Date and Rnd.Time - #29

--- src/RndF/Rnd.cs
+++ src/RndF/Rnd.cs
@@ -12,12 +12,6 @@ namespace RndF;
 public static partial class Rnd
 {
 	/// <summary>
-	/// Generate a random DateTime between the year 0 and the year 9999
-	/// </summary>
-	public static DateTime DateTime =>
-		DateTimeF.Get();
-
-	/// <summary>
 	/// 'Flip a coin' - generate a random true / false
 	/// </summary>
 	public static bool Flip =>
@@ -30,6 +24,56 @@ public static partial class Rnd
 	public static Guid Guid =>
 		GuidF.Get();
 #pragma warning restore CA1720 // Identifier contains type name
+
+	/// <summary>
+	/// Generate a random passphrase with eight dictionary words all starting with an uppercase
+	/// letter, and one number
+	/// </summary>
+	public static Maybe<string> Pass =>
+		StringF.Passphrase();
+
+	/// <summary>
+	/// Generate a random string 6 characters long, containing uppercase and lowercase letters,
+	/// but no numbers or special characters
+	/// </summary>
+	public static string Str =>
+		StringF.Get(6);
+
+	#region DateTime
+
+	/// <summary>
+	/// Generate a random Date between the year 1 and the year 9999
+	/// </summary>
+	public static DateOnly Date =>
+		DateF.Get();
+
+	/// <summary>
+	/// Generate a random DateTime (UTC) between the year 1 and the year 9999
+	/// </summary>
+	public static DateTime DateTime =>
+		DateTimeF.Get();
+
+	/// <summary>
+	/// Generate a random Time between 00:00 and 23:59
+	/// </summary>
+	public static TimeOnly Time =>
+		TimeF.Get();
+
+	#endregion DateTime
+
+	#region Numbers
+
+	/// <summary>
+	/// Generate a random double-precision floating-point number between 0 and 10000
+	/// </summary>
+	public static double Dbl =>
+		NumberF.GetDouble(max: 10000d);
+
+	/// <summary>
+	/// Generate a random single-precision floating-point number between 0 and 10000
+	/// </summary>
+	public static float Flt =>
+		NumberF.GetSingle(max: 10000f);
 
 	/// <summary>
 	/// Generate a random 32-bit integer between 0 and 10000
@@ -46,32 +90,20 @@ public static partial class Rnd
 		NumberF.GetInt64(max: 10000L);
 
 	/// <summary>
-	/// Generate a random passphrase with eight dictionary words all starting with an uppercase
-	/// letter, and one number
-	/// </summary>
-	public static Maybe<string> Pass =>
-		StringF.Passphrase();
-
-	/// <summary>
-	/// Generate a random string 6 characters long, containing uppercase and lowercase letters,
-	/// but no numbers or special characters
-	/// </summary>
-	public static string Str =>
-		StringF.Get(6);
-
-	/// <summary>
 	/// Generate a random 32-bit unsigned integer between 0 and 10000
 	/// </summary>
 #pragma warning disable CA1720 // Identifier contains type name
-	public static uint Uint =>
+	public static uint UInt =>
 		NumberF.GetUInt32(max: 10000u);
 #pragma warning restore CA1720 // Identifier contains type name
 
 	/// <summary>
 	/// Generate a random 64-bit unsigned integer between 0 and 10000
 	/// </summary>
-	public static ulong Ulng =>
+	public static ulong ULng =>
 		NumberF.GetUInt64(max: 10000UL);
+
+	#endregion Numbers
 
 	#region Classes
 
