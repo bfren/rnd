0a42f947edcde171d1840a02a35b32aa2da485e4
bfren <bf@bfren.dev>
2026-01-29 10:33:29 +0000

    Adding Int128 tests - #179

---
+++ tests/Tests.RndF/Functions/NumberF/GetInt128_Tests.cs
@@ -0,0 +1,57 @@
+// Rnd: Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2021
+
+namespace RndF.Rnd_Tests.NumberF_Tests;
+
+public class GetInt128_Tests
+{
+	public class without_args
+	{
+		[Fact]
+		public void never_returns_out_of_bounds() =>
+			Helpers.CheckBounds(() => Rnd.NumberF.GetInt128(), 0, Int128.MaxValue);
+	}
+
+	public class with_max
+	{
+		public static TheoryData<Int128> Max =>
+			new() { { Rnd.Int32 } };
+
+		[Theory]
+		[MemberData(nameof(Max))]
+		public void never_returns_out_of_bounds(Int128 max) =>
+			Helpers.CheckBounds(max => Rnd.NumberF.GetInt128(max), 0, max);
+	}
+
+	public class with_min_and_max
+	{
+		public class when_min_is_more_than_max
+		{
+			[Fact]
+			public void throws_MaximumNotMoreThanMinimumException() =>
+				Helpers.MaximumLessThanMinimum(nameof(Rnd.NumberF.GetInt128), () => Rnd.Int128, Rnd.NumberF.GetInt128);
+		}
+
+		public class when_min_is_less_than_zero
+		{
+			[Fact]
+			public void throws_MinimumLessThanZeroException() =>
+				Helpers.MinimumLessThanZero(nameof(Rnd.NumberF.GetInt128), () => Rnd.Int128 * -1, () => Rnd.Int128, Rnd.NumberF.GetInt128);
+		}
+
+		public static TheoryData<Int128, Int128> MinAndMax
+		{
+			get
+			{
+				var min = Rnd.Int32;
+				var max = min + 1 + Rnd.Int32;
+				return new() { { min, max } };
+			}
+		}
+
+		[Theory]
+		[MemberData(nameof(MinAndMax))]
+		public void never_returns_out_of_bounds(Int128 min, Int128 max) =>
+			Helpers.CheckBounds((min, max) => Rnd.NumberF.GetInt128(min, max), min, max);
+	}
+}
