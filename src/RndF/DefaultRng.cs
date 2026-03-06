b60142ea74075e7f782e5d35135ff065280dd14f
bfren <bf@bfren.dev>
2026-03-06 09:30:13 +0000

    Removing unnecessary stackalloc - #212

--- src/RndF/DefaultRng.cs
+++ src/RndF/DefaultRng.cs
@@ -1,7 +1,6 @@
 // Rnd: Random value generators.
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2021
 
-using System;
 using System.Security.Cryptography;
 
 namespace RndF;
@@ -11,20 +10,9 @@ namespace RndF;
 /// </summary>
 public sealed class DefaultRng : IRng
 {
-	private const int StackAllocThreshold = 256;
-
 	/// <inheritdoc/>
 	public byte[] GetBytes(int length)
 	{
-		// Check threshold to avoid stack overflow
-		if (length <= StackAllocThreshold)
-		{
-			Span<byte> byteSpan = stackalloc byte[length];
-			RandomNumberGenerator.Fill(byteSpan);
-			return byteSpan.ToArray();
-		}
-
-		// Safely get more bytes than StackAllocThreshold
 		var byteArray = new byte[length];
 		RandomNumberGenerator.Fill(byteArray);
 		return byteArray;
